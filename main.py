802d728d1f68fc83c5fc10d609ab9fe78bc1a6e2
Guilherme <gscavalcante02@gmail.com>
2026-08-26 21:14:40 -0300

    style: format main.py using ruff

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
 from database.connection import init_db
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Runs when the application starts 
+    # Runs when the application starts
     init_db()
     yield
-    # Runs when the application shuts down 
+    # Runs when the application shuts down
+
 
 app = FastAPI(title="E-commerce API", lifespan=lifespan)
+
 
 @app.get("/")
 def read_root():
